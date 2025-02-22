ccd8f27ce68f29302e503c8e77b1df90876080a3
Sagar Agola <35369309+sagar-agola@users.noreply.github.com>
2025-02-22 09:31:33 +0530

    more prettier and EsLint changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,17 @@
-import {SplashScreen, Stack} from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 import "./globals.css";
 import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
-  const [ fontsLoaded ] = useFonts({
+  const [fontsLoaded] = useFonts({
     "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
     "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
     "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
     "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
     "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
-    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf")
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
   });
 
   useEffect(() => {
@@ -25,8 +25,8 @@ export default function RootLayout() {
   }
 
   return (
-      <GlobalProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </GlobalProvider>
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GlobalProvider>
   );
 }
