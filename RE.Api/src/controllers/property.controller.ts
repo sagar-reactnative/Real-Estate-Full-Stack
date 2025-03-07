233bf7ec803002ffd1f33fa7597c514c147f45f9
Sagar Agola <35369309+sagar-agola@users.noreply.github.com>
2025-03-07 11:16:34 +0530

    added endpoint for get all properties

--- RE.Api/src/controllers/property.controller.ts
+++ RE.Api/src/controllers/property.controller.ts
@@ -4,6 +4,46 @@ import { Property } from '../database/models/property.model';
 import { PropertyDetailModel } from '../models/property/property-detail.model';
 import { PropertyListModel } from '../models/property/property-list.model';
 import { AgentDetailModel } from '../models/agent/agent-detail.model';
+import { Category } from '../database/models/category.model';
+
+const getAll = async (req: Request, res: Response) => {
+    const limit: number = Number(req.query.limit) ?? 10;
+
+    const filterQuery: any = {};
+    if (req.query.category && req.query.category != 'All') {
+        filterQuery.category = await Category.findOne({ title: req.query.category }).select('_id');
+    }
+
+    if (req.query.query) {
+        filterQuery.$or = [
+            { title: { $regex: req.query.query, $options: 'i' } },
+            { address: { $regex: req.query.query, $options: 'i' } },
+            { description: { $regex: req.query.query, $options: 'i' } }
+        ];
+    }
+
+    const properties = await Property.find(filterQuery).populate('reviews').populate('category').sort({ createdAt: -1 }).limit(limit);
+
+    const mappedProperties: PropertyListModel[] = properties.map(property => {
+        const images: string[] = property.images.map(image => image.url);
+
+        const mappedProperty: PropertyListModel = {
+            id: property.id,
+            title: property.title,
+            address: property.address,
+            imageUrl: images.length > 0 ? images[0] : null,
+            price: property.price,
+            averageRating: property.reviews.reduce((sum, item) => sum + item.rating, 0) / property.reviews.length
+        };
+
+        return mappedProperty;
+    });
+
+    res.status(200).json({
+        success: true,
+        data: mappedProperties
+    });
+};
 
 const getLatest = async (req: Request, res: Response) => {
     const properties = await Property.find().populate('reviews').sort({ createdAt: -1 }).limit(5);
@@ -111,6 +151,7 @@ const mapAgentObject = (agent: any): AgentDetailModel => {
 };
 
 export const PropertyController = {
+    getAll,
     getLatest,
     getById
 };
--- RE.Api/src/routes/property.routes.ts
+++ RE.Api/src/routes/property.routes.ts
@@ -3,6 +3,7 @@ import { PropertyController } from '../controllers/property.controller';
 
 const router: Router = express.Router();
 
+router.get('/', PropertyController.getAll);
 router.get('/latest', PropertyController.getLatest);
 router.get('/:id', PropertyController.getById);
 
